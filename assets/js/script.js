4799cfc7225ea556c1b8ac1c6796c9c97341a2bd
Sam Wallace <sam@sammymayhem.com>
2022-04-06 23:55:00 -0700

    added functions for changing element class. testing the if.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY"));
 
-var past = document.querySelector("description");
+var hour = document.querySelectorAll(".description");
+var hourNow = moment().hour();
+var plannerHour = document.getElementById("now").textContent;
+
+console.log(hour);
+
+
+if (hourNow > plannerHour) {
+    pastClass();
+} else if (hourNow === plannerHour) {
+    presentClass();
+} else {
+    futureClass();
+}
+
+// [0].className
+
+function pastClass() {
+    for (const past of hour) {
+        past.classList.remove("present", "future")
+        past.classList.add("past");
+    }
+}
+
+function presentClass() {
+    for (const present of hour) {
+        present.classList.remove("future", "past");
+        present.classList.add("present");
+    }
+}
+
+function futureClass() {
+    for (const future of hour) {
+        future.classList.remove("past", "present");
+        future.classList.add("future");
+    }
+}
+
 
 
