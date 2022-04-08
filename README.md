eff0509c1f5f1e8f838d120376501b9063951892
Sam Wallace <sam@sammymayhem.com>
2022-04-07 23:51:07 -0700

    finished README.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# totally-working-planner
+# Totally Working Planner
+
+## Description
+Welcome to the Totally Working Planner! I think the most important aspect of this is...it totally works. This project was a challenging one, at this point which one isn't? However, I learned a lot about JQuery and Moment JS while constructing this beheamoth of organization. On top of that, learning the functionality of Bootstrap has been enlightening as well!
+
+(When I write these, I am not sure if the comedy is appreciated, but I feel it add's flavor anyways, but let me know if I should hold off :) )
+
+Do you want to get your day on track? Do you want to stay organized beyond belief? Well you've come to the right place! Do you have something to do at 9am? Perfect! Throw it on the Totally Working Planner. How about 3pm? Bam! We got you! Got an important meeting at 1:15pm? Why not set your meeting for 1pm and be the go getter that was ready early! Here at Totally Working Planner our day is wide open, for you.
+
+## Link
+
+https://sammymayhem.github.io/totally-working-planner/
+
+## Screenshot
+
+![screenshot of the totally working planner homepage](assets/Images/homepage.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,29 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY"));
 
-var hourNow = moment().format("[hour-]H");
-var timeID = $(".time-block").attr("id");
 var time = $(".time-block");
 var hour = parseInt(currentDay.format("H"));
 var saveBtn = $(".saveBtn");
-var planText = document.querySelector(".description");
+var planText = $(".description").val();
+var planTime = $(".time-block").attr("id");
+var planSaved = [];
 
 // savedPlan();
-
-// Changes input box background every hour
-// if (timeID < hourNow) {
-//     pastClass();
-// } else if (timeID === hourNow) {
-//     presentClass();
-// } else if (timeID > hourNow){
-//     futureClass();
-// } 
+console.log(planText);
+console.log(planTime);
 
 // local storage
-saveBtn.on("click", function() {
+saveBtn.on("click", function(event) {
     var button = $(this);
     var planText = $(".description").val();
     var planTime = button.closest(".time-block").attr("id");
     localStorage.setItem("time", planTime);
     localStorage.setItem("plan", planText);
 })
+
+// if (!planTime || !planText) {
+//     return;
+// }
 
 // function savedPlan (){
 //     var time = localStorage.getItem("time");
@@ -64,72 +61,4 @@ function addClass() {
 
 addClass();
 
-// function getSavedPlan (){
-//     var planTime = localStorage.getItem("time");
-//     var planText = localStorage.getItem("plan");
-//     if (!localStorage.getItem("plan")) {
-//         return "";
-//     } 
-      
-// }
 
-// getSavedPlan();
-
-
-
-
-// for (var i = 0; i < 10; i++) {
-//     if (timeID < hourNow) {
-//         pastClass(past);
-//     } else if (timeID === hourNow) {
-//         presentClass(present);
-//     } else {
-//         futureClass(future);
-//     }
-// }
-
-// function pastClass() {
-//     $(".time-block").each(function () {
-//         $(this).removeClass("future", "present")
-//         $(this).addClass("past");
-//     });
-// }
-
-// function presentClass() {
-//     $(".time-block").each(function () {
-//         $(this).removeClass("future", "past")
-//         $(this).addClass("present");
-//     });
-// }
-
-// function futureClass() {
-//     $(".time-block").each(function () {
-//         $(this).removeClass("present", "past")
-//         $(this).addClass("future");
-//     });
-// }
-
-
-
-// function pastClass(past) {
-//         past.classList.remove("present", "future");
-//         past.classList.add("past");
-// }
-
-// function presentClass(present) {
-//         present.classList.remove("future", "past");
-//         present.classList.add("present");
-// }
-
-// function futureClass(future) {
-//         future.classList.remove("past", "present");
-//         future.classList.add("future");
-// }
-
-
-
-// Get values from local storage
-
-// Put them in local storage on button click (one for each button)
-
-//loop through each element, for each element, get that hour, compare to current hour, add class
